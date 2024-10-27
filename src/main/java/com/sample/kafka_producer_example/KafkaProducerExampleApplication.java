15d87a826c587de6dd94a7dc663c304bb0739081
Amit Kumar <amitstude3801@gmail.com>
2024-10-27 12:42:49 +0530

    Kafka Producer

---
+++ src/main/java/com/sample/kafka_producer_example/KafkaProducerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.kafka_producer_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerExampleApplication.class, args);
+	}
+
+}
